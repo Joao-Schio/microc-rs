447f950257299b625bc11a6d1df41c4477621425
Joao Pedro Schio Ortega <105325867+Joao-Schio@users.noreply.github.com>
2026-09-15 19:00:07 -0400

    Test parser error line message

---
+++ src/tests/parser/error.rs
@@ -0,0 +1,19 @@
+use crate::{
+    parser::Parser,
+    token::{Token, TokenType},
+};
+
+#[test]
+fn parser_error_message_includes_source_line() {
+    let tokens = vec![Token::new(TokenType::Plus, 7, b"+".to_vec())];
+    let mut parser = Parser::new(&tokens);
+
+    let error = parser
+        .parse_expression()
+        .expect_err("plus token should not parse as an expression");
+
+    assert_eq!(
+        error.to_string(),
+        "expected expression, found Plus at line 7"
+    );
+}
