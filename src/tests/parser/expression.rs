08dbf1ab863370ecf2ff4708a0fe15ffeed570dc
Joao Pedro Schio Ortega <105325867+Joao-Schio@users.noreply.github.com>
2026-09-15 17:36:46 -0400

    test: cover parser error for unexpected tokens

--- src/tests/parser/expression.rs
+++ src/tests/parser/expression.rs
@@ -1,18 +1,14 @@
-use crate::{parser::{Parser, ast::expression::Expression}, token::{Token, TokenType}};
+use crate::{
+    ast::expression::Expression,
+    parser::{Parser, ParserError},
+    token::{Token, TokenType},
+};
 
 #[test]
 fn parses_integer_expression() {
     let tokens = vec![
-        Token::new(
-            TokenType::IntegerConst(42),
-            1,
-            b"42".to_vec(),
-        ),
-        Token::new(
-            TokenType::Eof,
-            1,
-            vec![],
-        ),
+        Token::new(TokenType::IntegerConst(42), 1, b"42".to_vec()),
+        Token::new(TokenType::Eof, 1, vec![]),
     ];
 
     let mut parser = Parser::new(&tokens);
@@ -21,8 +17,21 @@ fn parses_integer_expression() {
         .parse_expression()
         .expect("integer expression should parse");
 
+    assert_eq!(expression, Expression::Integer(42));
+}
+
+#[test]
+fn non_expression_token_returns_parser_error() {
+    let tokens = vec![Token::new(TokenType::Plus, 1, b"+".to_vec())];
+    let mut parser = Parser::new(&tokens);
+
+    let result = parser.parse_expression();
+
     assert_eq!(
-        expression,
-        Expression::Integer(42)
+        result,
+        Err(ParserError::UnexpectedToken {
+            expected: "expression",
+            found: TokenType::Plus,
+        })
     );
 }
