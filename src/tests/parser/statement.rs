8b793f1f41e5f39061d21e4557ab6ba4db3aa5e1
Joao Pedro Schio Ortega <105325867+Joao-Schio@users.noreply.github.com>
2026-09-18 15:15:43 -0400

    Add return statement parser tests

--- src/tests/parser/statement.rs
+++ src/tests/parser/statement.rs
@@ -127,3 +127,119 @@ fn parses_consecutive_assignments() {
     assert!(parser.parse_statement().is_ok());
     assert!(parser.parse_statement().is_ok());
 }
+
+#[test]
+fn parses_return_without_value() {
+    let tokens = vec![
+        Token::new(TokenType::Return, 1, b"return".to_vec()),
+        Token::new(TokenType::SemiColon, 1, b";".to_vec()),
+        Token::new(TokenType::Eof, 1, vec![]),
+    ];
+
+    let mut parser = Parser::new(&tokens);
+
+    assert_eq!(
+        parser.parse_statement(),
+        Ok(Statement::Return { value: None })
+    );
+}
+
+#[test]
+fn parses_return_with_value() {
+    let tokens = vec![
+        Token::new(TokenType::Return, 1, b"return".to_vec()),
+        Token::new(TokenType::IntegerConst(42), 1, b"42".to_vec()),
+        Token::new(TokenType::SemiColon, 1, b";".to_vec()),
+        Token::new(TokenType::Eof, 1, vec![]),
+    ];
+
+    let mut parser = Parser::new(&tokens);
+
+    assert_eq!(
+        parser.parse_statement(),
+        Ok(Statement::Return {
+            value: Some(Expression::Integer(42)),
+        })
+    );
+}
+
+#[test]
+fn parses_expression_in_return() {
+    let tokens = vec![
+        Token::new(TokenType::Return, 1, b"return".to_vec()),
+        Token::new(TokenType::Id, 1, b"x".to_vec()),
+        Token::new(TokenType::Plus, 1, b"+".to_vec()),
+        Token::new(TokenType::Id, 1, b"foo".to_vec()),
+        Token::new(TokenType::Lparen, 1, b"(".to_vec()),
+        Token::new(TokenType::IntegerConst(1), 1, b"1".to_vec()),
+        Token::new(TokenType::Rparen, 1, b")".to_vec()),
+        Token::new(TokenType::SemiColon, 1, b";".to_vec()),
+        Token::new(TokenType::Eof, 1, vec![]),
+    ];
+
+    let mut parser = Parser::new(&tokens);
+
+    assert_eq!(
+        parser.parse_statement(),
+        Ok(Statement::Return {
+            value: Some(Expression::Binary {
+                left: Box::new(Expression::Identifier(b"x".to_vec())),
+                op: BinaryOp::Add,
+                right: Box::new(Expression::Call {
+                    callee: b"foo".to_vec(),
+                    arguments: vec![Expression::Integer(1)],
+                }),
+            }),
+        })
+    );
+}
+
+#[test]
+fn return_requires_semicolon() {
+    let tokens = vec![
+        Token::new(TokenType::Return, 1, b"return".to_vec()),
+        Token::new(TokenType::IntegerConst(42), 1, b"42".to_vec()),
+        Token::new(TokenType::Eof, 1, vec![]),
+    ];
+
+    let mut parser = Parser::new(&tokens);
+
+    assert_eq!(
+        parser.parse_statement(),
+        Err(ParserError::UnexpectedToken {
+            expected: "';'",
+            found: TokenType::Eof,
+            line: 1,
+        })
+    );
+}
+
+#[test]
+fn parses_assignment_followed_by_return() {
+    let tokens = vec![
+        Token::new(TokenType::Id, 1, b"x".to_vec()),
+        Token::new(TokenType::Assign, 1, b"=".to_vec()),
+        Token::new(TokenType::IntegerConst(1), 1, b"1".to_vec()),
+        Token::new(TokenType::SemiColon, 1, b";".to_vec()),
+        Token::new(TokenType::Return, 2, b"return".to_vec()),
+        Token::new(TokenType::Id, 2, b"x".to_vec()),
+        Token::new(TokenType::SemiColon, 2, b";".to_vec()),
+        Token::new(TokenType::Eof, 2, vec![]),
+    ];
+
+    let mut parser = Parser::new(&tokens);
+
+    assert_eq!(
+        parser.parse_statement(),
+        Ok(Statement::Assignment {
+            target: AssignmentTarget::Identifier(b"x".to_vec()),
+            value: Expression::Integer(1),
+        })
+    );
+    assert_eq!(
+        parser.parse_statement(),
+        Ok(Statement::Return {
+            value: Some(Expression::Identifier(b"x".to_vec())),
+        })
+    );
+}
