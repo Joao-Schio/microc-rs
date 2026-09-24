0d7bef4e9a448e1a38ed73a56f81fe073719e850
Joao Pedro Schio Ortega <105325867+Joao-Schio@users.noreply.github.com>
2026-09-24 12:12:08 -0400

    refactor: distinguish main from generic functions

--- src/ast/program.rs
+++ src/ast/program.rs
@@ -2,14 +2,20 @@ use crate::ast::statement::{Block, Type};
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Program {
-    pub functions: Vec<FunctionDefinition>,
+    pub functions: Vec<GenericFunction>,
+    pub main: MainFunction,
 }
 
 #[derive(Debug, PartialEq, Eq)]
-pub struct FunctionDefinition {
+pub struct GenericFunction {
     pub return_type: Type,
     pub name: Vec<u8>,
     pub parameters: Vec<Parameter>,
+    pub body: Block,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct MainFunction {
     pub body: Block,
 }
 
