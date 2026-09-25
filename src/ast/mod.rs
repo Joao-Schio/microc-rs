969b5fd77763a57cf19d2cb3b0d45cc17c0aaf49
Joao Pedro Schio Ortega <105325867+Joao-Schio@users.noreply.github.com>
2026-09-25 16:03:44 -0400

    fix: preserve identifier source line

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -2,28 +2,23 @@ pub mod expression;
 pub mod program;
 pub mod statement;
 
-#[derive(Debug, PartialEq, Eq, Clone, Hash)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub struct Identifier {
-    bytes: Vec<u8>,
+    pub name: Vec<u8>,
+    pub line: usize,
 }
 
 impl Identifier {
-    pub fn new(bytes: Vec<u8>) -> Self {
-        Self { bytes }
+    pub fn new(name: Vec<u8>, line: usize) -> Self {
+        Self { name, line }
     }
 
     pub fn as_bytes(&self) -> &[u8] {
-        &self.bytes
+        &self.name
     }
 
     pub fn into_bytes(self) -> Vec<u8> {
-        self.bytes
-    }
-}
-
-impl From<Vec<u8>> for Identifier {
-    fn from(bytes: Vec<u8>) -> Self {
-        Self::new(bytes)
+        self.name
     }
 }
 
